a395af47d9ff15678ef4e3b706eb3b245868e951
privetyaarseniy <privetyaarseniy@gmail.com>
2023-10-22 00:07:39 +0300

    learn.unity second educational project

---
+++ Prototype2_Unity/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private readonly float cooldown = 3f;
+    private float timer;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = cooldown;
+            }
+        }
+    }
+}
